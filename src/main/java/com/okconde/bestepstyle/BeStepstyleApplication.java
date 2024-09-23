e209f5d16a40128bd03cd670990d7e06b3094b16
DaThuTapCode <ntpdth2004@gmail.com>
2024-09-23 21:13:38 +0700

    Trong Phu cau hinh co so du an

---
+++ src/main/java/com/okconde/bestepstyle/BeStepstyleApplication.java
@@ -0,0 +1,15 @@
+package com.okconde.bestepstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStepstyleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BeStepstyleApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/okconde/bestepstyle/BeStepstyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okconde.bestepstyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeStepstyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
