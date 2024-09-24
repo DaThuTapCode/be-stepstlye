568a9c2ba6f195fdd5fd8dea412a3051bc9bf954
DaThuTapCode <ntpdth2004@gmail.com>
2024-09-24 17:08:02 +0700

    Trong Phu cau hinh not SPCT

--- src/main/java/com/okconde/bestepstyle/core/entity/SanPhamChiTiet.java
+++ src/main/java/com/okconde/bestepstyle/core/entity/SanPhamChiTiet.java
@@ -39,7 +39,30 @@ public class SanPhamChiTiet {
     @JoinColumn(name = "id_san_pham")
     private SanPham sanPham;
 
+    @ManyToOne
+    @JoinColumn(name = "id_chat_lieu")
+    private ChatLieu chatLieu;
+
+    @ManyToOne
+    @JoinColumn(name = "id_kieu_de_giay")
+    private KieuDeGiay kieuDeGiay;
+
+    @ManyToOne
+    @JoinColumn(name = "id_chat_lieu_de_giay")
+    private ChatLieuDeGiay chatLieuDeGiay;
+
+    @ManyToOne
+    @JoinColumn(name = "id_trong_luong")
+    private TrongLuong trongLuong;
+
+    @ManyToOne
+    @JoinColumn(name = "id_mau_sac")
+    private MauSac mauSac;
+
+    @ManyToOne
+    @JoinColumn(name = "id_kich_co")
+    private KichCo kichCo;
+
     @OneToMany(mappedBy = "sanPhamChiTiet")
     private List<Anh> anhs;
-
 }
