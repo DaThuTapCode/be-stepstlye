b989b3ca6e95a20f47908fd11e849797c2978b92
QuangMinhhhhhhh <vuquangminh0307@gmail.com>
2024-09-25 20:55:21 +0700

    QMinh cau hinh cn nv_kh

---
+++ src/main/java/com/okconde/bestepstyle/feature/employeemanagement/service/ChucVuService.java
@@ -0,0 +1,9 @@
+package com.okconde.bestepstyle.feature.employeemanagement.service;
+
+/**
+ * Created by Quang Minh on 9/25/2024 20:26:58
+ *
+ * @author Quang Minh
+ */
+public class ChucVuService {
+}
---
+++ src/main/java/com/okconde/bestepstyle/feature/employeemanagement/service/DiaChiKhachHangService.java
@@ -0,0 +1,9 @@
+package com.okconde.bestepstyle.feature.employeemanagement.service;
+
+/**
+ * Created by Quang Minh on 9/25/2024 20:27:27
+ *
+ * @author Quang Minh
+ */
+public class DiaChiKhachHangService {
+}
