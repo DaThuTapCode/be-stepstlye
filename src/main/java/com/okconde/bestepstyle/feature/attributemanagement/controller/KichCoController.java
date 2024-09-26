cbb3ca587f631f154df85f6375e9e2209f9a1a1d
ngogiatu <xdcap12345@gmail.com>
2024-09-26 09:45:47 +0700

    ngo tu them get all kich co

--- src/main/java/com/okconde/bestepstyle/feature/attributemanagement/controller/KichCoController.java
+++ src/main/java/com/okconde/bestepstyle/feature/attributemanagement/controller/KichCoController.java
@@ -1,9 +1,32 @@
 package com.okconde.bestepstyle.feature.attributemanagement.controller;
 
+import com.okconde.bestepstyle.core.dto.kichco.reponse.KichCoResponse;
+import com.okconde.bestepstyle.core.objecthttp.ResponseData;
+import com.okconde.bestepstyle.feature.attributemanagement.service.KichCoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * Created at 25/09/2024 by Ngo Tu
  *
  * @author: Ngo Tu
  */
+@RestController
+@RequestMapping("api/kich-co")
 public class KichCoController {
+    private final KichCoService kichCoService;
+
+    public KichCoController(KichCoService kichCoService) {
+        this.kichCoService = kichCoService;
+    }
+
+    @GetMapping("get-all")
+    public ResponseEntity<ResponseData<List<KichCoResponse>>> getAllKichCo(){
+        return ResponseEntity.ok(new ResponseData(HttpStatus.OK.value(), "Lấy thành công kích cỡ!",kichCoService.getAll()));
+    }
 }
--- src/main/java/com/okconde/bestepstyle/feature/attributemanagement/service/KichCoService.java
+++ src/main/java/com/okconde/bestepstyle/feature/attributemanagement/service/KichCoService.java
@@ -1,9 +1,61 @@
 package com.okconde.bestepstyle.feature.attributemanagement.service;
 
+import com.okconde.bestepstyle.core.dto.kichco.reponse.KichCoResponse;
+import com.okconde.bestepstyle.core.dto.kichco.request.KichCoRequest;
+import com.okconde.bestepstyle.core.entity.KichCo;
+import com.okconde.bestepstyle.core.mapper.kichco.response.KichCoResponseMapper;
+import com.okconde.bestepstyle.core.repository.KichCoRepository;
+import com.okconde.bestepstyle.core.service.IBaseService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created at 25/09/2024 by Ngo Tu
  *
  * @author: Ngo Tu
  */
-public class KichCoService {
+@Service
+public class KichCoService implements IBaseService<KichCo, Long, KichCoRequest, KichCoResponse> {
+    private final KichCoRepository kichCoRepository;
+
+    private final KichCoResponseMapper kichCoResponseMapper;
+
+    public KichCoService(KichCoRepository kichCoRepository, KichCoResponseMapper kichCoResponseMapper) {
+        this.kichCoRepository = kichCoRepository;
+        this.kichCoResponseMapper = kichCoResponseMapper;
+    }
+
+
+    @Override
+    public List<KichCoResponse> getPage(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<KichCoResponse> getAll() {
+        List<KichCo> kichCoList = kichCoRepository.findAll();
+        return kichCoResponseMapper.listToDTO(kichCoList);
+    }
+
+    @Override
+    public KichCoResponse create(KichCoRequest kichCoRequest) {
+        return null;
+    }
+
+    @Override
+    public KichCoResponse update(Long aLong, KichCoRequest kichCoRequest) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
+
+    @Override
+    public KichCoResponse getById(Long aLong) {
+        return null;
+    }
 }
