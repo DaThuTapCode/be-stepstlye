ac988a8d37f3092125f75fbcdb5c991ac135afd4
ngogiatu <xdcap12345@gmail.com>
2024-09-26 08:17:35 +0700

    ngo tu them get all chat lieu

--- src/main/java/com/okconde/bestepstyle/feature/attributemanagement/controller/ChatLieuController.java
+++ src/main/java/com/okconde/bestepstyle/feature/attributemanagement/controller/ChatLieuController.java
@@ -1,9 +1,33 @@
 package com.okconde.bestepstyle.feature.attributemanagement.controller;
 
+import com.okconde.bestepstyle.core.dto.chatlieu.response.ChatLieuResponse;
+import com.okconde.bestepstyle.core.objecthttp.ResponseData;
+import com.okconde.bestepstyle.feature.attributemanagement.service.ChatLieuService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * Created at 25/09/2024 by Ngo Tu
  *
  * @author: Ngo Tu
  */
+@RestController
+@RequestMapping("api/chat-lieu")
 public class ChatLieuController {
+
+    private final ChatLieuService chatLieuService;
+
+    public ChatLieuController(ChatLieuService chatLieuService) {
+        this.chatLieuService = chatLieuService;
+    }
+
+    @GetMapping("get-all")
+    public ResponseEntity<ResponseData<List<ChatLieuResponse>>> getAllChatLieu(){
+        return ResponseEntity.ok(new ResponseData(HttpStatus.OK.value(),"Lấy dữ liệu chất liệu thành công!", chatLieuService.getAll()));
+    }
 }
--- src/main/java/com/okconde/bestepstyle/feature/attributemanagement/service/ChatLieuService.java
+++ src/main/java/com/okconde/bestepstyle/feature/attributemanagement/service/ChatLieuService.java
@@ -1,9 +1,64 @@
 package com.okconde.bestepstyle.feature.attributemanagement.service;
 
+import com.okconde.bestepstyle.core.dto.chatlieu.request.ChatLieuRequest;
+import com.okconde.bestepstyle.core.dto.chatlieu.response.ChatLieuResponse;
+import com.okconde.bestepstyle.core.entity.ChatLieu;
+import com.okconde.bestepstyle.core.mapper.chatlieu.response.ChatLieuResponseMapper;
+import com.okconde.bestepstyle.core.repository.ChatLieuRepository;
+import com.okconde.bestepstyle.core.service.IBaseService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 /**
  * Created at 25/09/2024 by Ngo Tu
  *
  * @author: Ngo Tu
  */
-public class ChatLieuService {
+@Service
+public class ChatLieuService implements IBaseService<ChatLieu, Long, ChatLieuRequest, ChatLieuResponse> {
+    private final ChatLieuRepository chatLieuRepository;
+
+    private final ChatLieuResponseMapper chatLieuResponseMapper;
+
+    public ChatLieuService(
+            ChatLieuRepository chatLieuRepository,
+            ChatLieuResponseMapper chatLieuResponseMapper
+    ) {
+        this.chatLieuRepository = chatLieuRepository;
+        this.chatLieuResponseMapper = chatLieuResponseMapper;
+    }
+
+
+    @Override
+    public List<ChatLieuResponse> getPage(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<ChatLieuResponse> getAll() {
+        List<ChatLieu> chatLieuList = chatLieuRepository.findAll();
+        return chatLieuResponseMapper.listToDTO(chatLieuList);
+    }
+
+    @Override
+    public ChatLieuResponse create(ChatLieuRequest chatLieuRequest) {
+        return null;
+    }
+
+    @Override
+    public ChatLieuResponse update(Long aLong, ChatLieuRequest chatLieuRequest) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long aLong) {
+
+    }
+
+    @Override
+    public ChatLieuResponse getById(Long aLong) {
+        return null;
+    }
 }
